59bac68f09cc38e8a879d5322150694f10a6b498
Micaela Saez <mica_saez@hotmail.com>
2017-02-01 12:25:55 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -4,10 +4,10 @@ function Mostar()
 {
 	//esto es una comentario de una linea w3school github
 	var nombre; //defino variable, y cargo el dato abajo
-	nombre="Micaela"; //cargo dato
-	nombre=prompt("ingresa su nombre", "natalianatalia")
-	alert(" nombre "+nombre); //con comillas es valor literal,sin comilas es el valor de la variable. La mezcla es concatenar
-	var person = prompt("ingresa tu nombre", "natalianatalia");
+	nombre="Micaela"; //cargo dato variable
+	nombre=prompt("ingresa su nombre", "natalianatalia") //prompt no pede ir solo, propmt define la variable
+ 	alert(" nombre "+nombre); //con comillas es valor literal,sin comilas es el valor de la variable. La mezcla es concatenar
+	var person = prompt("ingresa tu nombre", "natalianatalia"); // linea de w3school
 	
 }
 
