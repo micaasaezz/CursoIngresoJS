1977ecfcde3d40b81233314e65710fa45e8f1749
Micaela Saez <mica_saez@hotmail.com>
2017-02-05 18:36:36 -0300

    falta la tercer parte
    falta hacer el promedio

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,64 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var preciouno;
+	var preciodos;
+	var preciotres;
 	
+	preciouno=document.getElementById('PrecioUno').value;
+	preciodos=document.getElementById('PrecioDos').value;
+	preciotres=document.getElementById('PrecioTres').value;
+
+	preciouno=parseInt(preciouno);
+	preciodos=parseInt(preciodos);
+	preciotres=parseInt(preciotres);
+
+	suma=preciouno+preciodos+preciotres;
+
+	alert(suma);
+
 }
+
 function Promedio () 
 {
+	var preciouno;
+	var preciodos;
+	var preciotres;
 	
+	preciouno=document.getElementById('PrecioUno').value;
+	preciodos=document.getElementById('PrecioDos').value;
+	preciotres=document.getElementById('PrecioTres').value;
+
+	preciouno=parseInt(preciouno);
+	preciodos=parseInt(preciodos);
+	preciotres=parseInt(preciotres);
+
+	suma=preciouno+preciodos+preciotres;
+	promedio=suma/3;
+
+	alert(promedio);
+
 }
 function PrecioFinal () 
 {
+	var preciouno;
+	var preciodos;
+	var preciotres;
 	
+	preciouno=document.getElementById('PrecioUno').value;
+	preciodos=document.getElementById('PrecioDos').value;
+	preciotres=document.getElementById('PrecioTres').value;
+
+	preciouno=parseInt(preciouno);
+	preciodos=parseInt(preciodos);
+	preciotres=parseInt(preciotres);	
+
+	suma=preciouno+preciodos+preciotres;
+	
+	/*
+	promedio=suma+21%
+
+	falta sacar el promedio
+	*/
+
 }
