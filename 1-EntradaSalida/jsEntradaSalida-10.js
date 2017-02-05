1aba5aaa21c80a2c768c96d6b149ad4e9bb615f7
Micaela Saez <mica_saez@hotmail.com>
2017-02-05 17:42:13 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+
+	importe=document.getElementById('importe').value;
+
+	importe=parseInt(importe);
+
+	Descuento=importe*25/100;
+    Descuento=parseInt(Descuento);
+
+	resultado=importe-Descuento;
+
+	alert(resultado);
+
+
 }
