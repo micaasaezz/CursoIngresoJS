9e962f422adb07056bfe6dde869f8bf1ebd80e8f
Micaela Saez <mica_saez@hotmail.com>
2017-02-12 20:17:22 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -5,7 +5,19 @@ function Mostrar()
 
 	//alert (laHora);
 
-	
+	switch (laHora)
+	{
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
+			alert("Es de mañana.");
+			break;
+
+		
+
+	}
 		
 	
 
