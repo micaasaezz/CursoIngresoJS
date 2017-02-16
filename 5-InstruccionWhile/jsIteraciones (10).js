85654f6afcebf10d918edd1740e2b5113675b40f
Micaela Saez <mica_saez@hotmail.com>
2017-02-16 19:55:11 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -4,10 +4,13 @@ function Mostrar()
 	var contador=0;
 	//declarar contadores y variables
 	var numero;
-	var sumaNegativos;
-	var sumaPositivos;
+	var sumaNegativos=0;
+	var sumaPositivos=0;
 	var acumuladorPositivos=0;
 	var acumuladorNegativos=0;
+	var contCero=0;
+	var contNumPares=0;
+	//var contNumImpares=0;
 	
 	var respuesta="si";
 
@@ -32,22 +35,30 @@ function Mostrar()
 				contCero++;
 			}
 		}
+
 		if(numero%2==0){
-
+			contNumPares++;
 		} //num par
-		else{}//num impar
+		/*
+		else{
+			contNumImpares++;
+		}//num impar
+		*/
 
-		
-	
-	
+
 
 		respuesta = prompt("Ingresar otro numero? si/no");
 	
 	}
 
-	promedio=????
+	promedioPos = sumaPositivos/acumuladorPositivos;
+	promedioNeg = sumaNegativos/acumuladorNegativos;
 
- 	document.write("hola");
+	diferencia = sumaPositivos - sumaNegativos;
+
+ 	document.write("La suma de los negativos es "+sumaNegativos+". La suma de los positivos es "+sumaPositivos+". La cantidad de positivos es "+acumuladorPositivos+". La cantidad de negativos es "+acumuladorNegativos+". La cantidad de ceros es "+contCero+". La cantidad de numeros pares es "+contNumPares+". El promedio de numeros positivos es "+promedioPos+". El promedio de numeros negativos es "+promedioNeg+". La diferencia entre numeros positivos y numeros negativos es "+diferencia+".");
+
+
 
 
 }//FIN DE LA FUNCIÓN
