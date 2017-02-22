402a56dc70966959b4f54aac60b152024ce4b5d5
Micaela Saez <mica_saez@hotmail.com>
2017-02-22 12:16:34 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,21 @@
 function Mostrar()
 {
+	var numero;
+	var i;
+	var cantidadPar=0;
 
+	numero = prompt("Ingrese número.");
+	numero = parseInt(numero);
 
+	for(i=1 ; i<=numero ; i++){
+		if(i%2==0){
+			document.write(i+"</br>");
+			cantidadPar++;
+		}
 
+		
+	}
+
+	document.write("La cantidad de pares es: "+cantidadPar );
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,22 @@
 function Mostrar()
 {
+	var numero;
+	var i;
+	var cantidadDiv=0;
 
+	
+
+	numero = prompt("Ingrese número.");
+	numero = parseInt(numero);
+
+	for(i=1 ; i<=numero ; i++){
+		if(numero%i==0){
+			document.write(i+"</br>");
+			cantidadDiv++;
+		}
+	}
+
+	document.write("La cantidad de divisores encontrados es "+cantidadDiv);
 
 
 
